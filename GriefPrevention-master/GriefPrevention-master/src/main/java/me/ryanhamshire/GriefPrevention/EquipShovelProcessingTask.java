3c41a4d36dbdc38cdd6c5572e90538646e8cc14d
jonahgilbertson <jonahgilbertson@helixmail.com>
2019-05-17 17:02:00 -0600

    Adding current working version with the /createclaim added and ommiting # of blocks required for claim

---
+++ GriefPrevention-master/GriefPrevention-master/src/main/java/me/ryanhamshire/GriefPrevention/EquipShovelProcessingTask.java
@@ -0,0 +1,42 @@
+/*
+    GriefPrevention Server Plugin for Minecraft
+    Copyright (C) 2012 Ryan Hamshire
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+package me.ryanhamshire.GriefPrevention;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+
+//tells a player about how many claim blocks he has, etc
+//implemented as a task so that it can be delayed
+//otherwise, it's spammy when players mouse-wheel past the shovel in their hot bars
+class EquipShovelProcessingTask implements Runnable 
+{
+	//player data
+	private Player player;
+	
+	public EquipShovelProcessingTask(Player player)
+	{
+		this.player = player;
+	}
+	
+	@Override
+	public void run()
+	{
+
+	}
+}
